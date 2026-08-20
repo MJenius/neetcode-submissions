6c8365e230021b2b460f157b20511674b2c11546
Mevin Jose <157293500+MJenius@users.noreply.github.com>
2026-08-20 22:18:20 +0530

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.java
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    private List<Integer> ans;
+    public List<Integer> postorder(Node root) {
+        ans=new ArrayList<>();
+        dfs(root);
+        return ans;
+    }
+    private void dfs(Node node) {
+        if (node==null) return;
+        for (Node child:node.children) dfs(child);
+        ans.add(node.val);
+    }
+}
